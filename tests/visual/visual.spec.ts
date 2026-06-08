92e29f736103c62b064718d0a0e22a2c4829f90a
kedarkpatil <kkpatil999@gmail.com>
2026-06-08 11:17:44 +0530

    Visual testing commented need to excecute seperately

--- tests/visual/visual.spec.ts
+++ tests/visual/visual.spec.ts
@@ -1,29 +1,29 @@
-import { test, expect } from '@playwright/test';
-import { LoginPage } from '../../page-objects/LoginPage';
-import { ProductsPage } from '../../page-objects/ProductsPage';
+// import { test, expect } from '@playwright/test';
+// import { LoginPage } from '../../page-objects/LoginPage';
+// import { ProductsPage } from '../../page-objects/ProductsPage';
 
-test.describe('Visual Regression Tests', () => {
+// test.describe('Visual Regression Tests', () => {
 
-    test('Inventory page visual validation', async ({ page }) => {
+//     test('Inventory page visual validation', async ({ page }) => {
 
-        const loginPage = new LoginPage(page);
-        const productsPage = new ProductsPage(page);
+//         const loginPage = new LoginPage(page);
+//         const productsPage = new ProductsPage(page);
 
-        await loginPage.navigate();
+//         await loginPage.navigate();
 
-        await loginPage.login(
-            'standard_user',
-            'secret_sauce'
-        );
+//         await loginPage.login(
+//             'standard_user',
+//             'secret_sauce'
+//         );
 
-        await productsPage.verifyProductsPage();
+//         await productsPage.verifyProductsPage();
 
-        await expect(page).toHaveScreenshot(
-            'inventory-page.png',
-            {
-                fullPage: true
-            }
-        );
-    });
+//         await expect(page).toHaveScreenshot(
+//             'inventory-page.png',
+//             {
+//                 fullPage: true
+//             }
+//         );
+//     });
 
-});
+// });
